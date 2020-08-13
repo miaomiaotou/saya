480692364b6604d64aff1694fdf6743fa5b0f8f5
miaomiaotou <aaaa>
2020-08-13 22:52:33 +0800

    loginright

--- src/views/User/update.vue
+++ src/views/User/update.vue
@@ -2,6 +2,7 @@
   <div >
    <!-- <Baselayout></Baselayout> -->
    <router-view></router-view>
+  
   </div>
 </template>
 
@@ -11,7 +12,23 @@ export default {
   name:"update",
   components:{
     Baselayout
-  }
+  },
+data() {
+    return {};
+  },
+ methods: {
+    handleChange(info) {
+      const status = info.file.status;
+      if (status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (status === 'done') {
+        this.$message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === 'error') {
+        this.$message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  },
   
 
 }
